b2940370de9d746544838c855d675b04661f52c7
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-02-09 01:32:55 +0600

    Initial commit

---
+++ api/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 8000;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cors());
+
+const run = async () => {
+
+
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port!`);
+    });
+
+};
+
+void run();
