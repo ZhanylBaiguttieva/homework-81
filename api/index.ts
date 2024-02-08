e66b919184879cc8bc89964c298d583921ea9a35
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-02-09 01:39:18 +0600

    Added index.ts with mongoose.db

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import config from './config';
 
 const app = express();
 const port = 8000;
@@ -9,11 +11,15 @@ app.use(express.json());
 app.use(cors());
 
 const run = async () => {
-
+    await mongoose.connect(config.mongoose.db);
 
     app.listen(port, () => {
         console.log(`Server started on ${port} port!`);
     });
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+    })
 
 };
 
