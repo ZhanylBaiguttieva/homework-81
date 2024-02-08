792e1bcc41688ba4a6821326a0459da0e9c02bdb
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-02-09 03:06:22 +0600

    Added links.ts

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import config from './config';
+import linksRouter from "./routers/links";
 
 const app = express();
 const port = 8000;
@@ -9,6 +10,8 @@ const port = 8000;
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
+
+app.use('/', linksRouter);
 
 const run = async () => {
     await mongoose.connect(config.mongoose.db);
---
+++ api/routers/links.ts
@@ -0,0 +1,51 @@
+import {Router} from "express";
+import Link from "../models/Link";
+import mongoose, {Types} from "mongoose";
+import {LinkObject} from "../types";
+
+const linksRouter = Router();
+
+linksRouter.get('/:shortUrl', async (req, res, next) => {
+    try {
+        const result = await Link.findOne({shortUrl:req.params.shortUrl});
+        if(!result){
+            res.status(404);
+        } else {
+            res.status(301).redirect(result.originalUrl);
+        }
+
+    } catch (e) {
+        return next(e);
+    }
+});
+
+linksRouter.post('/links', async (req, res, next) => {
+    function generateRandomString(length: number) {
+        const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        let randomString = '';
+        const charactersLength = characters.length;
+        for (let i = 0; i < length; i++) {
+            randomString += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return randomString;
+    }
+  try {
+    const linkData: LinkObject = {
+      shortUrl: generateRandomString(6),
+      originalUrl: req.body.originalUrl,
+    };
+
+    const link = new Link(linkData);
+    await link.save();
+
+    res.send(link);
+  } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError) {
+      return res.status(422).send(e);
+    }
+
+    next(e);
+  }
+});
+
+export default linksRouter;
