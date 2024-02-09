327b0fe594085db1cce714f97bc552489f5e5a21
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-02-09 16:57:43 +0600

    Added bootstrap

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import {Provider} from 'react-redux';
+import {store} from './app/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <Provider store={store}>
+        <App />
+      </Provider>
+  </React.StrictMode>
 );
